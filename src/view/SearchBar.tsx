520d89ea6b5c16ef097a45e07975728224704990
Pedro Henrique <pedro00dk@gmail.com>
2020-11-24 20:54:22 -0300

    Small change in search bar typografy and background

--- src/view/SearchBar.tsx
+++ src/view/SearchBar.tsx
@@ -2,9 +2,9 @@ import { css } from '@emotion/css'
 import * as React from 'react'
 
 const classes = {
-    container: `d-flex flex-column w-100 p-3 mb-3 shadow-sm ${css({ fontSize: '1.5em', background: 'white' })}`,
-    userInput: `border-0 ${css({ marginBottom: '-0.15em', fontSize: '1em', outline: 'none' })}`,
-    repositoryInput: `border-0 text-black-50 ${css({ fontSize: '0.6em', outline: 'none' })}`,
+    container: `d-flex flex-column w-100 p-3 shadow-sm bg-white`,
+    userInput: `border-0 ${css({ fontSize: '1.5em', marginBottom: '-0.15em', outline: 'none' })}`,
+    repositoryInput: `border-0 text-black-50 ${css({ outline: 'none' })}`,
 }
 
 /**
