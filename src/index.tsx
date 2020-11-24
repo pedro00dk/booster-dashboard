f3ad25907818005e92e5325124df0cc0b433be81
Pedro Henrique <pedro00dk@gmail.com>
2020-11-24 18:37:50 -0300

    Create project template

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import * as ReactDom from 'react-dom'
+import { Dashboard } from './view/Dashboard'
+
+const root$ = document.getElementById('root')
+ReactDom.render(<Dashboard />, root$)
---
+++ src/view/Dashboard.tsx
@@ -0,0 +1,3 @@
+import * as React from 'react'
+
+export const Dashboard = () => <div>Dashboard</div>
