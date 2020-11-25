ecc5b79b654969d167829391425fd0c34a27f7d5
Pedro Henrique <pedro00dk@gmail.com>
2020-11-25 17:30:23 -0300

    Implement the ColumnChart component

--- src/view/charts/ColumnChart.tsx
+++ src/view/charts/ColumnChart.tsx
@@ -1,42 +1,73 @@
 import * as Chart from 'chart.js'
 import * as React from 'react'
+
 const classes = {
     container: 'd-flex w-100 h-100',
 }
 
 Chart.defaults.global.defaultFontSize = 14
-console.log((Chart.defaults.global.defaultColor = ''))
 
-const chartOptions: Chart.ChartOptions = {
-    legend: { display: false },
-    responsive: true,
-    maintainAspectRatio: false,
-    scales: {
-        xAxes: [{ gridLines: { display: false } }],
-        yAxes: [
-            {
-                ticks: {
-                    beginAtZero: true, //
-                    callback: (value, index, values) => `${value}${value != 0 ? 'h' : ''}`,
+/**
+ * Dynamically compute chart options.
+ * The options that may change are related to maximum vertical axis value and tick step resolution.
+ *
+ * @param data chart data
+ */
+const computeChartOptions = (data: number[]): Chart.ChartOptions => {
+    const largestSample = Math.max(...data)
+    const roundToMax = largestSample > 72 ? 24 : largestSample > 48 ? 12 : largestSample > 24 ? 8 : 6
+    const max = largestSample > 24 ? Math.ceil(largestSample / roundToMax) * roundToMax : 24
+    const stepSize = max > 72 ? 24 : max > 48 ? 12 : max > 24 ? 8 : 6
+    const callback: Chart.NestedTickOptions['callback'] = value => `${value}${value != 0 ? 'h' : ''}`
+    return {
+        legend: { display: false },
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            xAxes: [{ gridLines: { display: false } }],
+            yAxes: [
+                {
+                    gridLines: { borderDash: [3, 1] },
+                    ticks: { beginAtZero: true, maxTicksLimit: 7, max, stepSize, callback },
                 },
-            },
-        ],
-    },
+            ],
+        },
+    }
 }
 
-export const ColumnChart = (props: { onSetData?: (setData: (labels: string[], data: number[]) => void) => void }) => {
+/**
+ * Create a chart.js dataset, which contains chart data and some rendering properties.
+ *
+ * @param data chart data
+ */
+const createDataset = (data: number[]) => ({
+    data,
+    backgroundColor: 'rgba(76, 155, 255)',
+    hoverBackgroundColor: 'rgba(76, 155, 255)',
+    barPercentage: 1,
+    categoryPercentage: 0.85,
+})
+
+/**
+ * Simple column chart component which minor tweaks to better display time data.
+ *
+ * @param props.setDataCallback callback that accepts a function to update the chart labels and values
+ */
+export const ColumnChart = (props: {
+    setDataCallback?: (setData: (labels: string[], data: number[]) => void) => void
+}) => {
     const canvas$ = React.useRef<HTMLCanvasElement>()
     const chart = React.useRef<Chart>()
 
     React.useLayoutEffect(() => {
-        chart.current = new Chart.Chart(canvas$.current.getContext('2d'), { type: 'bar', options: chartOptions })
-        props.onSetData?.(setData)
-        setData(['Small', 'Medium', 'Large'], [20, 32, 45])
+        chart.current = new Chart.Chart(canvas$.current.getContext('2d'), { type: 'bar' })
+        props.setDataCallback?.(setData)
     }, [])
 
-    const setData = (labels: string[], data: number[], color = '#5d83ff') => {
+    const setData = (labels: string[], data: number[]) => {
         chart.current.data.labels = labels
-        chart.current.data.datasets = [{ data, backgroundColor: color, hoverBackgroundColor: color }]
+        chart.current.data.datasets = [createDataset(data)]
+        chart.current.options = computeChartOptions(data)
         chart.current.update()
     }
 
