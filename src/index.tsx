eeb4e5e3f37089bca13cef4c48ca74f24bd76027
Pedro Henrique <pedro00dk@gmail.com>
2020-11-26 00:16:36 -0300

    Fix a problem with the bundler and babel polyfills

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import * as ReactDom from 'react-dom'
+import 'regenerator-runtime/runtime'
 import { Dashboard } from './view/Dashboard'
 
 const root$ = document.getElementById('root')
