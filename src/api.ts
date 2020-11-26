221d90af68fdd42a3a77fbe983f2789330129190
Pedro Henrique <pedro00dk@gmail.com>
2020-11-26 00:13:24 -0300

    API fetchRepositoryData now collects both pull requests and issues

--- src/api.ts
+++ src/api.ts
@@ -17,10 +17,37 @@ const headers = {
  * @param body graphql body
  * @returns a object containing the abort function and the request promise
  */
-const sendGraphqlRequest = (body: string) => {
-    const { abort, signal } = new AbortController()
-    const promise = fetch(api, { method: 'POST', headers, body, signal })
-    return { abort, promise }
+const graphqlFetch = (body: string) => {
+    const abortController = new AbortController()
+    const promise = fetch(api, { method: 'POST', headers, body, signal: abortController.signal })
+    return { promise, abort: () => abortController.abort() }
+}
+
+/**
+ * Helper function to fetch large graphql collections using cursors with abortion support.
+ *
+ * @param buildQuery function to build a query that only takes the current cursor
+ * @param getPageInfo obtain the pageInfo from the current iteration result
+ * @param stopPredicate checks if process should stop based on the current result
+ * @param signal abort signal for cancellation purposes
+ */
+const graphqlFetchCollection = async (
+    buildQuery: (cursor: string) => string,
+    getPageInfo: (result: any) => { hasNextPage: boolean; endCursor: string },
+    stopPredicate: (result: any) => boolean,
+    signal?: AbortSignal
+) => {
+    const results = []
+    let cursor = 'null'
+    while (true) {
+        const response = await fetch(api, { method: 'POST', headers, signal, body: buildQuery(cursor) })
+        const result = await response.json()
+        results.push(result)
+        const pageInfo = getPageInfo(result)
+        if (!pageInfo.hasNextPage || stopPredicate(result)) break
+        cursor = `"${pageInfo.endCursor}"`
+    }
+    return results
 }
 
 /**
@@ -45,48 +72,92 @@ export const searchUsersAndOrganizations = (partialName: string) => {
         }
         `,
     })
-    return sendGraphqlRequest(body)
+    return graphqlFetch(body)
 }
 
 /**
- * Request pull request and issues information from the provided repository, up to the provided startDate.
+ * Fetch issues and pull requests from the provided repository until a issue or pull request creation date reaches
+ * startDate.
+ * This function fetches issues and pull requests in different queries which run in parallel.
+ * It would be more complicated to track two cursors for different collections in the same query.
  *
  * @param username user or organization name
  * @param repository repository name
- * @param startDate start date of pull requests
- * @returns list of pull requests
+ * @param startDate start date of issues and pull requests
+ * @returns promise that yields issues and pull requests, and the abort trigger
  */
-export const queryRepositoryData = async (username: string, repository: string, startDate: Date) => {
-    const queryBuilder = (cursor: string) =>
-        JSON.stringify({
-            query: `
-        query {
-            repository( owner: "${username}", name: "${repository}") {
-                pullRequests(first: 25, orderBy: {field: CREATED_AT, direction: DESC}, after: ${cursor}) {
-                    edges {
-                        node {
-                            createdAt closedAt mergedAt changedFiles
-                            commits { totalCount }
-                        }
-                    }
-                    pageInfo { endCursor hasNextPage }
+export const fetchRepositoryData = (username: string, repository: string, startDate: Date) => {
+    const abortController = new AbortController()
+    const issuesPromise = graphqlFetchCollection(
+        fetchIssuesQuery(username, repository),
+        result => result.data.repository.issues.pageInfo,
+        result => {
+            const lastIssue = result.data.repository.issues.edges.slice(-1)[0].node
+            return new Date(lastIssue.createdAt).getTime() - startDate.getTime() < 0
+        },
+        abortController.signal
+    )
+    const pullRequestsPromise = graphqlFetchCollection(
+        fetchPullRequestsQuery(username, repository),
+        result => result.data.repository.pullRequests.pageInfo,
+        result => {
+            const lastPullRequest = result.data.repository.pullRequests.edges.slice(-1)[0].node
+            return new Date(lastPullRequest.createdAt).getTime() - startDate.getTime() < 0
+        },
+        abortController.signal
+    )
+    const promise = Promise.all([issuesPromise, pullRequestsPromise]).then(results => {
+        const issues = results[0]
+            .flatMap(result => result.data.repository.issues.edges)
+            .map(issue => issue.node)
+            .map(({ createdAt, closedAt }) => ({
+                createdAt: createdAt && new Date(createdAt),
+                closedAt: closedAt && new Date(closedAt),
+            }))
+        const pullRequests = results[1]
+            .flatMap(result => result.data.repository.pullRequests.edges)
+            .map(pullRequest => pullRequest.node)
+            .map(({ createdAt, closedAt, mergedAt, changedFiles, commits }) => ({
+                createdAt: createdAt && new Date(createdAt),
+                closedAt: closedAt && new Date(closedAt),
+                mergedAt: mergedAt && new Date(mergedAt),
+                changedFiles: changedFiles as number,
+                commits: commits.totalCount as number,
+            }))
+        return { issues, pullRequests }
+    })
+    return { promise, abort: () => abortController.abort() }
+}
+
+// graphql query builders
+
+const fetchIssuesQuery = (username: string, repository: string) => (cursor: string) =>
+    JSON.stringify({
+        query: `
+query {
+    repository( owner: "${username}", name: "${repository}") {
+        issues(first: 100, orderBy: {field: CREATED_AT, direction: DESC}, after: ${cursor}) {
+            edges { node { createdAt closedAt } }
+            pageInfo { endCursor hasNextPage }
+        }
+    }
+}`,
+    })
+
+const fetchPullRequestsQuery = (username: string, repository: string) => (cursor: string) =>
+    JSON.stringify({
+        query: `
+query {
+    repository( owner: "${username}", name: "${repository}") {
+        pullRequests(first: 50, orderBy: {field: CREATED_AT, direction: DESC}, after: ${cursor}) {
+            edges {
+                node {
+                    createdAt closedAt mergedAt changedFiles
+                    commits { totalCount }
                 }
             }
-        }`,
-        })
-    const pullRequests = []
-    let cursor = 'null'
-    while (true) {
-        const promise = fetch(api, { method: 'POST', headers, body: queryBuilder(cursor) })
-        const response = await promise
-        const content = await response.json()
-        pullRequests.push(...content.data.repository.pullRequests.edges.map(pullRequest => pullRequest.node))
-        const pageInfo = content.data.repository.pullRequests.pageInfo
-        cursor = `"${pageInfo.endCursor}"`
-        if (!pageInfo.hasNextPage) break
-        const oldestPullRequest = pullRequests[pullRequests.length - 1]
-        const beforeStartDate = new Date(oldestPullRequest.createdAt).getTime() - startDate.getTime() < 0
-        if (beforeStartDate) break
+            pageInfo { endCursor hasNextPage }
+        }
     }
-    return pullRequests
-}
+}`,
+    })
