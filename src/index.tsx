5f86abefdc3071eefd90e621a4f24f65f9d388da
Pedro Henrique <pedro00dk@gmail.com>
2020-11-28 15:09:05 -0300

    Remove default margin added to the body tag

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
+import { css } from '@emotion/css'
 import * as React from 'react'
 import * as ReactDom from 'react-dom'
 import 'regenerator-runtime/runtime'
 import { Dashboard } from './view/Dashboard'
+
+// override default user agent stylesheet properties
+document.getElementsByTagName('body').item(0).className = css({ margin: 0, padding: 0 })
 
 const root$ = document.getElementById('root')
 ReactDom.render(<Dashboard />, root$)
