a108f54e60ccd7678e8d4aadd18c33ff9c641687
Pedro Henrique <pedro00dk@gmail.com>
2020-11-29 15:18:31 -0300

    Add aditional number checking cases in TimeDisplay component

--- src/view/TimeDisplay.tsx
+++ src/view/TimeDisplay.tsx
@@ -17,20 +17,24 @@ const classes = {
  *
  * @param props.time time to be displayed
  */
-export const TimeDisplay = (props: { time: number }) => {
+export const TimeDisplay = (props: { time?: number }) => {
     /**
      * Format time to string.
      *
      * @param time time to format
      */
     const timeToDisplayString = (time: number) => {
-        if (isNaN(time)) return 'unavailable'
+        if (props.time == undefined) return '\u200b' // vertical space character
+        if (typeof time !== 'number' || isNaN(time)) return 'Not a Number'
+        if (!isFinite(time)) return time.toString()
+        const sign = Math.sign(time)
+        time = Math.abs(time)
         const minutes = Math.floor((time / (1000 * 60)) % 60)
         const hours = Math.floor((time / (1000 * 60 * 60)) % 24)
         const days = Math.floor(time / (1000 * 60 * 60 * 24))
         const dayString = days > 0 ? `${days}day${days != 1 ? 's' : ''}` : ''
         const hourString = `${hours}h${minutes < 10 ? '0' : ''}${minutes}m`
-        return `${dayString} ${hourString}`.trim()
+        return `${dayString} ${hourString} ${sign < 0 ? 'remaining' : ''}`.trim()
     }
 
     return <span className={classes.container}>{timeToDisplayString(props.time)}</span>
