a3d2ffcd450e91f6c88860569ac1cd046e6d8c92
Pedro Henrique <pedro00dk@gmail.com>
2023-12-28 01:16:23 -0300

    Start conversion to solid-js

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,3 @@
-import { css } from '@emotion/css'
-import * as React from 'react'
-import * as ReactDom from 'react-dom'
-import 'regenerator-runtime/runtime'
-import './stores/issues'
-import { Dashboard } from './view/Dashboard'
+import { render } from 'solid-js/web'
 
-// override default user agent stylesheet properties
-document.getElementsByTagName('body').item(0).className = css({ margin: 0, padding: 0 })
-
-const root$ = document.getElementById('root')
-ReactDom.render(<Dashboard />, root$)
+render(() => <div>hello</div>, document.body)
