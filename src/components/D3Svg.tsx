e92fcd60c465ca0d55c2e0154a5cf798e2651931
Pedro Henrique <pedro00dk@gmail.com>
2023-12-29 18:59:55 -0300

    Add container div to D3Svg

--- src/components/D3Svg.tsx
+++ src/components/D3Svg.tsx
@@ -1,13 +1,21 @@
 import { Accessor, JSX, createSignal, onCleanup, onMount } from 'solid-js'
+import classes from './D3Svg.module.scss'
 
 export const D3Svg = (
-    props: JSX.IntrinsicElements['svg'] & { draw?: (ref: SVGSVGElement, size: Accessor<ResizeObserverSize>) => void },
+    props: JSX.IntrinsicElements['div'] & {
+        draw?: (div: HTMLDivElement, svg: SVGSVGElement, size: Accessor<ResizeObserverSize>) => void
+    },
 ) => {
-    let ref: SVGSVGElement = undefined as any
+    let div: HTMLDivElement = undefined as any
+    let svg: SVGSVGElement = undefined as any
     const [size, setSize] = createSignal<ResizeObserverSize>({ blockSize: 0, inlineSize: 0 })
     const observer = new ResizeObserver(([{ contentBoxSize }]) => setSize(contentBoxSize[0] ?? size()))
-    onMount(() => (observer.observe(ref), props.draw?.(ref, size)))
+    onMount(() => (observer.observe(svg), props.draw?.(div, svg, size)))
     onCleanup(observer.disconnect)
 
-    return <svg {...props} ref={ref} />
+    return (
+        <div {...props} ref={div} class={`${classes.root} ${props.class ?? ''}`}>
+            <svg ref={svg} />
+        </div>
+    )
 }
