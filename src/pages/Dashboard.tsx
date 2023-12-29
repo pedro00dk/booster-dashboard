ce1bb8920753aeeda7b40dd193593a4bea092d8f
Pedro Henrique <pedro00dk@gmail.com>
2023-12-29 18:29:58 -0300

    Finish dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,69 @@
-import { createMemo, createSignal } from 'solid-js'
+import { UTCDate } from '@date-fns/utc'
+import { useSearchParams } from '@solidjs/router'
+import * as d3 from 'd3'
+import { endOfDay, startOfDay, subMonths } from 'date-fns'
+import { Accessor, createComputed, createMemo, createRenderEffect, createSignal } from 'solid-js'
 import { Card } from '../components/Card'
+import { D3Svg } from '../components/D3Svg'
 import { SearchBar } from '../components/SearchBar'
 import { Tabs } from '../components/Tabs'
-import { LineChart } from '../components/charts/LineChart'
-import { Issue, averageCloseTimeMillis, averageMergeTimeMillis, issuesActions } from '../stores/issues'
+import {
+    Issue,
+    averageCloseTimeMillis,
+    averageMergeTimeMillis,
+    dailySummaryDatasets,
+    issuesActions,
+} from '../stores/issues'
 import classes from './Dashboard.module.scss'
 
-const millisToDuration = (millis: number = 0) => {
+export const Dashboard = () => {
+    const [search, setSearch] = useSearchParams<Partial<{ tab: string }>>()
+    const tab = () => (search.tab ?? 'prs') as 'prs' | 'issues'
+    const today = endOfDay(new UTCDate())
+    const oneMonthAgo = startOfDay(subMonths(new UTCDate(), 1))
+    const [allIssues, setIssues] = createSignal<Issue[]>()
+    const prs = createMemo(() => allIssues()?.filter(({ pull_request }) => pull_request))
+    const issues = createMemo(() => allIssues()?.filter(({ pull_request }) => !pull_request))
+    const averageMergeTime = createMemo(() => prs() && averageMergeTimeMillis(prs()!, oneMonthAgo, today))
+    const averageCloseTime = createMemo(() => issues() && averageCloseTimeMillis(issues()!, oneMonthAgo, today))
+    const dailySummaryPrs = createMemo(() => {
+        const summary = prs() && dailySummaryDatasets(prs()!, oneMonthAgo, today)
+        return summary
+    })
+    const dailySummaryIssues = createMemo(() => {
+        const summary = issues() && dailySummaryDatasets(issues()!, oneMonthAgo, today)
+        return summary
+    })
+    const dailySummary = () => (tab() === 'prs' ? dailySummaryPrs() : dailySummaryIssues())
+
+    const onSubmit = async (owner: string, repo: string) => setIssues(await issuesActions.fetchIssues(owner, repo))
+
+    return (
+        <article class={classes.root}>
+            <Card>
+                <SearchBar onSubmit={onSubmit} />
+            </Card>
+            <div class={classes.cols}>
+                <Card title='Average PR merge time'>
+                    <span class={classes.duration}>{millisToDuration(averageMergeTime())}</span>
+                </Card>
+                <Card title='Average issue close time'>
+                    <span class={classes.duration}>{millisToDuration(averageCloseTime())}</span>
+                </Card>
+            </div>
+            <Card title='Month summary'>
+                <Tabs class={classes.tabs} tab={tab()} onTab={tab => setSearch({ tab })}>
+                    <span data-tab='prs'>PRs</span>
+                    <span data-tab='issues'>Issues</span>
+                </Tabs>
+                <D3Svg class={classes.chart} draw={(...args) => dailySummaryChart(...args, dailySummary)} />
+            </Card>
+        </article>
+    )
+}
+
+const millisToDuration = (millis?: number) => {
+    if (millis == undefined) return `\u{200b}`
     if (isNaN(millis)) return 'Not a Number'
     if (!isFinite(millis)) return millis.toString()
     const minutes = Math.abs(millis) / 60000
@@ -17,122 +74,58 @@ const millisToDuration = (millis: number = 0) => {
     return `${dayString} ${timeString} ${millis < 0 ? 'remaining' : ''}`
 }
 
-// issuesActions.fetchIssues('solidjs', 'solid-router')
+const dailySummaryChart = (
+    ref: SVGSVGElement,
+    size: Accessor<ResizeObserverSize>,
+    data: Accessor<ReturnType<typeof dailySummaryDatasets> | undefined>,
+) => {
+    const chart = d3.select(ref).append('g')
+    const lines = chart.append('g')
+    const scale = { x: d3.scaleUtc(), y: d3.scaleLinear() }
+    const axis = { x: chart.append('g'), y: chart.append('g') }
 
-export const Dashboard = () => {
-    // const [fetching, setFetching] = React.useState(false)
-    // const repositoryData = React.useRef<Issue[]>()
-    // const error = React.useRef<Error>()
-    // const abortSearch = React.useRef<() => void>()
-    const [issues, setIssues] = createSignal([] as Issue[])
-    const averageMergeTime = createMemo(() => averageMergeTimeMillis(issues()))
-    const averageCloseTime = createMemo(() => averageCloseTimeMillis(issues()))
+    // size changes
+    createComputed(() => {
+        const { inlineSize, blockSize } = size()
+        const pad = { l: 20, r: 10, t: 10, b: 20 }
+        const box = { w: inlineSize - pad.l - pad.r, h: blockSize - pad.t - pad.b }
+        chart.attr('transform', `translate(${pad.l},${pad.t})`)
+        scale.x.range([0, box.w])
+        scale.y.range([box.h, 0])
+    })
 
-    // // compute time ranges
-    // const today = new Date()
-    // today.setDate(today.getDate() + 1)
-    // today.setHours(0, 0, 0, -1)
-    // const oneMonthAgo = new Date(today)
-    // oneMonthAgo.setMonth(today.getMonth() - 1)
-    // oneMonthAgo.setHours(0, 0, 0, 0)
-    // const twoMonthsAgo = new Date(today)
-    // twoMonthsAgo.setMonth(today.getMonth() - 1)
-    // twoMonthsAgo.setHours(0, 0, 0, 0)
+    // data changes
+    createComputed(() => {
+        if (!data()) return
+        const { days, groups } = data()!
+        const xd = d3.extent(days) as [Date, Date]
+        const yd = [0, Object.values(groups).reduce((acc, next) => Math.max(acc, ...next), 1)]
+        scale.x.domain(xd)
+        scale.y.domain(yd)
+    })
 
-    // // memoize display and chart data computation to prevent recomputing when react redraws
-    // const { pullRequestMergeTime, issueCloseTime, createdPullRequests, createdIssues } = React.useMemo(() => {
-    //     if (fetching || repositoryData.current == undefined) return {}
-    //     const pullRequests = repositoryData.current.filter(({ pull_request }) => pull_request)
-    //     const issues = repositoryData.current.filter(({ pull_request }) => !pull_request)
-    //     return {
-    //         pullRequestMergeTime: metrics.computeAveragePullRequestMergeTime(pullRequests, oneMonthAgo, today),
-    //         issueCloseTime: metrics.computeAverageIssueCloseTime(issues, oneMonthAgo, today),
-    //         createdPullRequests: metrics.createdInRange(pullRequests, oneMonthAgo, today),
-    //         createdIssues: metrics.createdInRange(issues, oneMonthAgo, today),
-    //     }
-    // }, [fetching, repositoryData.current])
-    // // const { labels: columnChartLabels, datasets: columnChartDatasets } = React.useMemo(() => {
-    // //     return metrics.createPullRequestSizeDatasets(repositoryData.current, oneMonthAgo, today)
-    // // }, [fetching, repositoryData.current])
-    // // const { labels: lineChartLabels, pullRequestDatasets, issuesDatasets } = React.useMemo(() => {
-    // //     return metrics.createDaySummaryDatasets(repositoryData.current, oneMonthAgo, today)
-    // // }, [fetching, repositoryData.current])
+    // render axis
+    createRenderEffect(() => {
+        size()
+        data()
+        const w = scale.x.range()[1]
+        const h = scale.y.range()[0]
+        const ticks = { x: data() ? w / 80 : 0, y: data() ? Math.min(h / 40, scale.y.domain()[1]) : 0 }
+        axis.x.attr('transform', `translate(0,${h})`).call(d3.axisBottom(scale.x).ticks(ticks.x).tickSizeInner(-h))
+        axis.y.call(d3.axisLeft(scale.y).ticks(ticks.y).tickSizeInner(-w))
+    })
 
-    // /**
-    //  * Access the api to fetch repository data.
-    //  * This function is a callback passed to the SearchBar component.
-    //  * If some repository data is already being fetched, the request is aborted and a new request is sent.
-    //  *
-    //  * @param username github username
-    //  * @param repository user repository
-    //  */
-    // const searchCallback = async (username: string, repository: string) => {
-    //     abortSearch.current?.()
-    //     repositoryData.current = undefined
-    //     error.current = undefined
-    //     setFetching(true)
-    //     const issues = issuesActions.fetchIssues(username, repository)
-    //     repositoryData.current = await issues
-    //     setFetching(false)
-    // }
-
-    // console.log(fetching, error.current, repositoryData.current)
-    // const classes = {} as any
-
-    const onSubmit = async (owner: string, repo: string) => {
-        const issues = await issuesActions.fetchIssues(owner, repo)
-        setIssues(issues)
-    }
-
-    return (
-        <article class={classes.root}>
-            <Card>{<SearchBar onSubmit={onSubmit} />}</Card>
-            <Card title='Average merge time by pull request size'>{/* <ColumnChart /> */}</Card>
-            <div class={classes.cols}>
-                <Card title='Average pull request merge time'>
-                    <span class={classes.duration}>{millisToDuration(averageMergeTime())}</span>
-                </Card>
-                <Card title='Average issue close time'>
-                    <span class={classes.duration}>{millisToDuration(averageCloseTime())}</span>
-                </Card>
-            </div>
-            <Card title='Month summary'>
-                <Tabs class={classes.tabs} tab='prs'>
-                    <span data-tab='prs'>Pull requests</span>
-                    <span data-tab='issues'>Issues</span>
-                </Tabs>
-                <LineChart />
-            </Card>
-
-            {/* <div class={classes.header}>
-                    </div> */}
-            {/* {fetching && <div class={classes.spinner} />} */}
-            {/* <div class={classes.col}>
-                    <Card title='Average Merge Time by Pull Request Size'>
-                        <div style={{ flexGrow: 1, height: '28em', padding: '0em 1.5em' }}>
-                             <ColumnChart labels={columnChartLabels} datasets={columnChartDatasets} /> 
-                        </div>
-                    </Card>
-                    <div class={classes.row}>
-                        <Card title='Average Pull Request Merge Time'>
-                            <TimeDisplay time={pullRequestMergeTime} />
-                        </Card>
-                        <Card title='Average Issue Close Time'>
-                            <TimeDisplay time={issueCloseTime} />
-                        </Card>
-                    </div>
-                    <Card title='Month Summary'>
-                         <MonthSummary
-                            labels={lineChartLabels}
-                            tabs={[
-                                { name: 'Pull Requests', value: createdPullRequests, datasets: pullRequestDatasets },
-                                { name: 'Issues', value: createdIssues, datasets: issuesDatasets },
-                            ]}
-                        /> 
-                    </Card>
-                </div> */}
-            {/* <span style={{ height: '3.15em' }} /> */}
-            {/* </div> */}
-        </article>
-    )
+    // render lines
+    createRenderEffect(() => {
+        size()
+        if (!data()) return
+        const { days, groups, colors } = data()!
+        lines
+            .selectAll('path')
+            .data(Object.entries(groups))
+            .join('path')
+            .classed(classes.line, true)
+            .attr('stroke', ([group]) => colors[group])
+            .attr('d', ([, series]) => d3.line((_, i) => scale.x(days[i]), scale.y)(series))
+    })
 }
--- src/stores/issues.ts
+++ src/stores/issues.ts
@@ -1,3 +1,6 @@
+import { UTCDate } from '@date-fns/utc'
+import { eachDayOfInterval, startOfDay } from 'date-fns'
+
 export type User = {
     login: string
     avatar_url: string
@@ -14,88 +17,56 @@ export type Issue = {
     user: User
     labels: [{ name: string; description: string; color: string }]
     pull_request?: { html_url: string; merged_at?: Date }
-    created_at: Date
-    updated_at: Date
-    closed_at?: Date
+    created_at: UTCDate
+    updated_at: UTCDate
+    closed_at?: UTCDate
 }
 
 const fetchIssues = async (owner: string, repo: string) => {
-    const issues: Issue[] = await (await fetch(`https://api.github.com/repos/${owner}/${repo}/issues?state=all`)).json()
+    const api = 'https://api.github.com'
+    const url = (page: number) => `${api}/repos/${owner}/${repo}/issues?state=all&per_page=100&page=${page}`
+    const issues = (await Promise.all([1, 2, 3, 4].map(i => fetch(url(i)).then(r => r.json())))).flat() as Issue[]
     issues.forEach(issue => {
-        issue.created_at = new Date(issue.created_at)
-        issue.updated_at = new Date(issue.updated_at)
-        if (issue.closed_at) issue.closed_at = new Date(issue.closed_at)
-        if (issue.pull_request?.merged_at) issue.pull_request.merged_at = new Date(issue.pull_request.merged_at)
+        issue.created_at = new UTCDate(issue.created_at)
+        issue.updated_at = new UTCDate(issue.updated_at)
+        if (issue.closed_at) issue.closed_at = new UTCDate(issue.closed_at)
+        if (issue.pull_request?.merged_at) issue.pull_request.merged_at = new UTCDate(issue.pull_request.merged_at)
     })
     return issues
 }
 
 export const issuesActions = { fetchIssues }
 
-export const inRange = (issues: Issue[], from = new Date(-864e13), to = new Date(864e13)) =>
-    issues.filter(({ created_at }) => created_at >= from && created_at <= to)
+export const inRange = (issues: Issue[], start = new Date(-864e13), end = new Date(864e13)) =>
+    issues.filter(({ created_at }) => created_at >= start && created_at <= end)
 
-export const averageMergeTimeMillis = (issues: Issue[], from?: Date, to?: Date) =>
-    inRange(issues, from, to)
+export const averageMergeTimeMillis = (issues: Issue[], start?: Date, end?: Date) =>
+    inRange(issues, start, end)
         .filter(({ pull_request }) => pull_request?.merged_at)
         .map(({ created_at, pull_request }) => +pull_request!.merged_at! - +created_at)
         .reduce((acc, next, _, l) => acc + next / l.length, 0)
 
-export const averageCloseTimeMillis = (issues: Issue[], from?: Date, to?: Date) =>
-    inRange(issues, from, to)
+export const averageCloseTimeMillis = (issues: Issue[], start?: Date, end?: Date) =>
+    inRange(issues, start, end)
         .filter(({ pull_request, closed_at }) => !pull_request && closed_at)
         .map(({ created_at, closed_at }) => +closed_at! - +created_at)
         .reduce((acc, next, _, l) => acc + next / l.length, 0)
 
-// export const dailySummaryDatasets = (issues: Issue[], from: Date, to: Date) => {
-//     from.setHours(0, 0, 0, 0)
-//     to.setHours(0, 0, 0, 0)
-//     const dayMillis = 1000 * 60 * 60 * 24
-//     const days = [...Array((+to - +from) / dayMillis + 1)].map((_, i) => new Date(+from + dayMillis * i))
-//     const buckets: Record<, Issue[][]> { created: Issue[][]; closed: Issue[][]; merged: Issue[][] } = {
-//         created: [],
-//         closed: [],
-//         merged: [],
-//     }
-//     days.forEach((day, i, a) => {
-//         const next = a[i + 1] ?? to
-//         issues.filter(({ created_at }) => +day <= +created_at && +created_at < +next)
-//     })
-
-//     const dateToIndex = (date: Date) => {
-//         if (date == undefined) return -1
-//         const tempDate = new Date(date)
-//         tempDate.setHours(0, 0, 0, 0)
-//         return Math.round((tempDate.getTime() - from.getTime()) / (1000 * 60 * 60 * 24))
-//     }
-//     repositoryData.pullRequests.forEach(pullRequest => {
-//         const mergedAtIndex = dateToIndex(pullRequest.mergedAt)
-//         const createdAtIndex = dateToIndex(pullRequest.createdAt)
-//         const closedAtIndex = dateToIndex(pullRequest.closedAt)
-//         mergedPullRequests[mergedAtIndex] = (mergedPullRequests[mergedAtIndex] ?? 0) + 1
-//         createdPullRequests[createdAtIndex] = (createdPullRequests[createdAtIndex] ?? 0) + 1
-//         closedPullRequests[closedAtIndex] = (closedPullRequests[closedAtIndex] ?? 0) + 1
-//     })
-//     repositoryData.issues.forEach(issues => {
-//         const createdAtIndex = dateToIndex(issues.createdAt)
-//         const closedAtIndex = dateToIndex(issues.closedAt)
-//         createdIssues[createdAtIndex] = (createdIssues[createdAtIndex] ?? 0) + 1
-//         closedIssues[closedAtIndex] = (closedIssues[closedAtIndex] ?? 0) + 1
-//     })
-//     mergedPullRequests = mergedPullRequests.slice(0, days)
-//     createdPullRequests = createdPullRequests.slice(0, days)
-//     closedPullRequests = closedPullRequests.slice(0, days)
-//     createdIssues = createdIssues.slice(0, days)
-//     closedIssues = closedIssues.slice(0, days)
-
-//     const pullRequestDatasets = [
-//         { label: 'Merged', data: mergedPullRequests, color: '#b20bff' },
-//         { label: 'Opened', data: createdPullRequests, color: '#ff3a00' },
-//         { label: 'Closed', data: closedPullRequests, color: '#13c600' },
-//     ]
-//     const issuesDatasets = [
-//         { label: 'Opened', data: createdIssues, color: '#ff3a00' },
-//         { label: 'Closed', data: closedIssues, color: '#13c600' },
-//     ]
-//     return { labels, pullRequestDatasets, issuesDatasets }
-// }
+export const dailySummaryDatasets = (issues: Issue[], start: Date, end: Date) => {
+    const days = eachDayOfInterval({ start, end })
+    const [first, second] = days
+    const d = +second - +first
+    const groups: Record<string, number[]> = {
+        created: Array(days.length).fill(0),
+        closed: Array(days.length).fill(0),
+        merged: Array(days.length).fill(0),
+    }
+    const colors: Record<string, string> = { created: '#ff3a00', closed: '#13c600', merged: '#b20bff' }
+    issues.forEach(({ created_at, closed_at, pull_request }) => {
+        const merged_at = pull_request?.merged_at
+        if (created_at >= start && created_at < end) groups.created[(+startOfDay(created_at) - +first) / d]++
+        if (closed_at && closed_at >= start && closed_at < end) groups.closed[(+startOfDay(closed_at) - +first) / d]++
+        if (merged_at && merged_at >= start && merged_at < end) groups.merged[(+startOfDay(merged_at) - +first) / d]++
+    })
+    return { days, groups, colors }
+}
